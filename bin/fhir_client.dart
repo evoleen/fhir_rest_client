50eaa762856e58e901ef4c5c3d5df7d912e1cc49
Till Gerken <till.gerken@gmail.com>
2024-07-24 16:32:51 +0200

    first commit

---
+++ bin/fhir_client.dart
@@ -0,0 +1,5 @@
+import 'package:fhir_client/fhir_client.dart' as fhir_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fhir_client.calculate()}!');
+}
---
+++ test/fhir_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:fhir_client/fhir_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
